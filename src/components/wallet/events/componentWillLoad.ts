8344cfa9a1bf51527f6e84daae46deb2932b47db
Tyler van der Hoeven <hi@tyvdh.com>
2020-02-17 10:52:12 -0500

    updates

--- src/components/wallet/events/componentWillLoad.ts
+++ src/components/wallet/events/componentWillLoad.ts
@@ -8,7 +8,7 @@ export default async function componentWillLoad() {
 
     this.error = null
     this.server = new Server('https://horizon-testnet.stellar.org')
-    this.homeDomain = 'stellar-anchor-server.herokuapp.com'
+    this.homeDomain = 'testanchor.stellar.org'
     this.toml = await StellarTomlResolver.resolve(this.homeDomain)
 
     if (keystore) {
