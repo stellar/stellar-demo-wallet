24a94ed800cbfa2e2b6f26317398e8472b494da2
Michael Feldstein <msfeldstein@gmail.com>
2020-06-10 15:33:13 -0700

    Add ability to load a secret key into the wallet

--- src/components/wallet/wallet.ts
+++ src/components/wallet/wallet.ts
@@ -1,77 +1,72 @@
-import {
-  Component,
-  State,
-  Prop
-} from '@stencil/core'
-import {
-  Server,
-  ServerApi,
-} from 'stellar-sdk'
+import { Component, State, Prop } from "@stencil/core";
+import { Server, ServerApi } from "stellar-sdk";
 
-import componentWillLoad from './events/componentWillLoad' // UPDATE
-import render from './events/render' // UPDATE
+import componentWillLoad from "./events/componentWillLoad"; // UPDATE
+import render from "./events/render"; // UPDATE
 
-import createAccount from './methods/createAccount'
-import updateAccount from './methods/updateAccount'
-import depositAsset from './methods/depositAsset' // NEW
-import withdrawAsset from './methods/withdrawAsset' // NEW
-import trustAsset from './methods/trustAsset'
-import makePayment from './methods/makePayment'
-import copyAddress from './methods/copyAddress'
-import copySecret from './methods/copySecret'
-import signOut from './methods/signOut'
-import setPrompt from './methods/setPrompt'
+import createAccount from "./methods/createAccount";
+import updateAccount from "./methods/updateAccount";
+import depositAsset from "./methods/depositAsset"; // NEW
+import withdrawAsset from "./methods/withdrawAsset"; // NEW
+import trustAsset from "./methods/trustAsset";
+import makePayment from "./methods/makePayment";
+import copyAddress from "./methods/copyAddress";
+import copySecret from "./methods/copySecret";
+import signOut from "./methods/signOut";
+import setPrompt from "./methods/setPrompt";
+import loadAccount from "./methods/loadAccount";
 
-import { Prompter } from '@prompt/prompt'
+import { Prompter } from "@prompt/prompt";
 
 interface StellarAccount {
-  publicKey: string,
-  keystore: string,
-  state?: ServerApi.AccountRecord,
+  publicKey: string;
+  keystore: string;
+  state?: ServerApi.AccountRecord;
 }
 
 interface Loading {
-  fund?: boolean,
-  pay?: boolean,
-  trust?: boolean,
-  update?: boolean,
-  deposit?: boolean, // NEW
-  withdraw?: boolean // NEW
+  fund?: boolean;
+  pay?: boolean;
+  trust?: boolean;
+  update?: boolean;
+  deposit?: boolean; // NEW
+  withdraw?: boolean; // NEW
 }
 
 @Component({
-  tag: 'stellar-wallet',
-  styleUrl: 'wallet.scss',
-  shadow: true
+  tag: "stellar-wallet",
+  styleUrl: "wallet.scss",
+  shadow: true,
 })
 export class Wallet {
-  @State() account: StellarAccount
-  @State() prompter: Prompter = {show: false}
-  @State() loading: Loading = {}
-  @State() error: any = null
+  @State() account: StellarAccount;
+  @State() prompter: Prompter = { show: false };
+  @State() loading: Loading = {};
+  @State() error: any = null;
 
-  @Prop() server: Server
-  @Prop() homeDomain: String // NEW
-  @Prop() toml: Object // NEW
+  @Prop() server: Server;
+  @Prop() homeDomain: String; // NEW
+  @Prop() toml: Object; // NEW
 
   // Component events
   componentWillLoad() {}
   render() {}
 
   // Stellar methods
-  createAccount = createAccount
-  updateAccount = updateAccount
-  depositAsset = depositAsset // NEW
-  withdrawAsset = withdrawAsset // NEW
-  trustAsset = trustAsset
-  makePayment = makePayment
-  copyAddress = copyAddress
-  copySecret = copySecret
-  signOut = signOut
+  createAccount = createAccount;
+  loadAccount = loadAccount;
+  updateAccount = updateAccount;
+  depositAsset = depositAsset; // NEW
+  withdrawAsset = withdrawAsset; // NEW
+  trustAsset = trustAsset;
+  makePayment = makePayment;
+  copyAddress = copyAddress;
+  copySecret = copySecret;
+  signOut = signOut;
 
   // Misc methods
-  setPrompt = setPrompt
+  setPrompt = setPrompt;
 }
 
-Wallet.prototype.componentWillLoad = componentWillLoad
-Wallet.prototype.render = render
+Wallet.prototype.componentWillLoad = componentWillLoad;
+Wallet.prototype.render = render;
