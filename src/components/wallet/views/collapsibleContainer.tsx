e2de3d9cb039331ebbe57dae281b3af78a167ac5
Michael Feldstein <msfeldstein@gmail.com>
2020-06-17 17:08:13 -0700

    Proper button click handling

--- src/components/wallet/views/collapsibleContainer.tsx
+++ src/components/wallet/views/collapsibleContainer.tsx
@@ -6,7 +6,7 @@ export class CollapsibleContainer {
   @Prop() hideText: string = 'Hide'
   @Prop() showText: string = 'Show'
   @State() open: boolean
-  @Listen('click', { capture: true })
+
   handleClick() {
     this.open = !this.open
   }
@@ -14,7 +14,9 @@ export class CollapsibleContainer {
   render() {
     return (
       <div>
-        <button>{this.open ? this.hideText : this.showText}</button>
+        <button onClick={(e) => this.handleClick()}>
+          {this.open ? this.hideText : this.showText}
+        </button>
         <div style={{ display: this.open ? 'block' : 'none' }}>
           <slot></slot>
         </div>
