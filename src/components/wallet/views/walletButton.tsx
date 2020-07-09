3ba4abf9dea60451a0b71669345d0d6e4c8a7311
Michael Feldstein <msfeldstein@gmail.com>
2020-07-09 13:21:09 -0700

    Add missing file

---
+++ src/components/wallet/views/walletButton.tsx
@@ -0,0 +1,10 @@
+import { h } from '@stencil/core'
+
+export default function WalletButton(title, loadingKey, callback) {
+  const loader = this.loading[loadingKey] ? <stellar-loader /> : null
+  return (
+    <button onClick={() => callback()}>
+      {loader} {title}
+    </button>
+  )
+}
