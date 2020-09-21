4d4ddbdb0dd908220abd93413cd49018c7478b34
Tyler van der Hoeven <hi@tyvdh.com>
2020-09-21 11:19:26 -0400

    updates

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,39 +1,59 @@
-import { Config } from '@stencil/core'
-import { sass } from '@stencil/sass'
-import { postcss } from '@stencil/postcss'
-import autoprefixer from 'autoprefixer'
-import nodePolyfills from 'rollup-plugin-node-polyfills'
+import { Config } from "@stencil/core";
+import { sass } from "@stencil/sass";
+import { postcss } from "@stencil/postcss";
+import autoprefixer from "autoprefixer";
+import nodePolyfills from "rollup-plugin-node-polyfills";
 
 export const config: Config = {
-  namespace: 'stellar-wallet',
+  namespace: "stellar-wallet",
+  devServer: {
+    openBrowser: false,
+  },
   outputTargets: [
     {
-      type: 'dist',
-      esmLoaderPath: '../loader'
+      type: "dist",
+      esmLoaderPath: "../loader",
     },
     {
-      type: 'docs-readme'
+      type: "docs-readme",
     },
     {
-      type: 'www',
-      serviceWorker: null // disable service workers
-    }
+      type: "www",
+      serviceWorker: null, // disable service workers
+      baseUrl:
+        process.env.NODE_ENV === "development"
+          ? "http://localhost:3333/"
+          : "https://stellar-demo-wallet.now.sh/",
+    },
   ],
-  globalStyle: 'src/global/style.scss',
+  globalStyle: "src/global/style.scss",
   commonjs: {
     namedExports: {
-      'stellar-sdk': ['StrKey', 'xdr', 'Transaction', 'Keypair', 'Networks', 'Account', 'TransactionBuilder', 'BASE_FEE', 'Operation', 'Asset', 'Memo', 'MemoHash']
+      "stellar-sdk": [
+        "StrKey",
+        "xdr",
+        "Transaction",
+        "Keypair",
+        "Networks",
+        "Account",
+        "TransactionBuilder",
+        "BASE_FEE",
+        "Operation",
+        "Asset",
+        "Memo",
+        "MemoHash",
+      ],
     },
   },
   plugins: [
     nodePolyfills(),
     sass(),
     postcss({
-      plugins: [autoprefixer()]
-    })
+      plugins: [autoprefixer()],
+    }),
   ],
   nodeResolve: {
     browser: true,
-    preferBuiltins: true
-  }
-}
+    preferBuiltins: true,
+  },
+};
