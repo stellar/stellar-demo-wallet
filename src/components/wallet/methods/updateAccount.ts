284f5406b3553f8bbae3ee7e3b80cbbd392f8fc0
Lawrence Lawson <lawrence@stellar.org>
2020-11-05 15:06:33 -0800

    Move and rename claimableBalanceQuery
    -readability, readability, readability
    
    Signed-off-by: Lawrence Lawson <lawrence@stellar.org>

--- src/components/wallet/methods/updateAccount.ts
+++ src/components/wallet/methods/updateAccount.ts
@@ -7,27 +7,28 @@ import {
 import { handleError } from '@services/error'
 import { Wallet } from '../wallet'
 
+export function claimableBalanceQuery({ this: wallet }) {
+  wallet.server
+    .claimableBalances()
+    .claimant(wallet.account.publicKey)
+    .call()
+    .then(function (resp) {
+      resp.records.forEach((record) => {
+        wallet.logger.response(
+          'Claimable Balances Available ',
+          loOmit(record, ['_links', 'paging_token', 'self'])
+        )
+      })
+      console.log(resp)
+    })
+    .catch(function (err) {
+      wallet.error = handleError(err)
+      console.error(err)
+    })
+  wallet.loading = { ...wallet.loading, update: false }
+}
+
 export default async function updateAccount(this: Wallet) {
-  function cbQuery({ this: wallet }) {
-    wallet.server
-      .claimableBalances()
-      .claimant(wallet.account.publicKey)
-      .call()
-      .then(function (resp) {
-        resp.records.forEach((record) => {
-          wallet.logger.response(
-            'Claimable Balances Available ',
-            loOmit(record, ['_links', 'paging_token', 'self'])
-          )
-        })
-        console.log(resp)
-      })
-      .catch(function (err) {
-        wallet.error = handleError(err)
-        console.error(err)
-      })
-    wallet.loading = { ...wallet.loading, update: false }
-  }
   try {
     this.error = null
     this.loading = { ...this.loading, update: true }
@@ -46,7 +47,7 @@ export default async function updateAccount(this: Wallet) {
           ]),
         }
       })
-      .finally(() => cbQuery({ this: this }))
+      .finally(() => claimableBalanceQuery({ this: this }))
   } catch (err) {
     this.error = handleError(err)
   }
