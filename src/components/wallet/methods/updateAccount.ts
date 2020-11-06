1802936f7461aeeebbe4c4f2f947f17c47c71278
Lawrence Lawson <lawrence@stellar.org>
2020-11-06 09:11:17 -0800

    remove messy callbacks
    Signed-off-by: Lawrence Lawson <lawrence@stellar.org>

--- src/components/wallet/methods/updateAccount.ts
+++ src/components/wallet/methods/updateAccount.ts
@@ -8,46 +8,32 @@ import { handleError } from '@services/error'
 import { Wallet } from '../wallet'
 
 async function claimableBalanceQuery(wallet: Wallet) {
-  try {
-    let resp = await wallet.server
-      .claimableBalances()
-      .claimant(wallet.account.publicKey)
-      .call()
-    resp.records.forEach((record) => {
-      wallet.logger.response(
-        'Claimable Balances Available ',
-        loOmit(record, ['_links', 'paging_token', 'self'])
-      )
-    })
-  } catch (err) {
-    wallet.error = handleError(err)
-  }
+  let resp = await wallet.server
+    .claimableBalances()
+    .claimant(wallet.account.publicKey)
+    .call()
+  resp.records.forEach((record) => {
+    wallet.logger.response(
+      'Claimable Balances Available ',
+      loOmit(record, ['_links', 'paging_token', 'self'])
+    )
+  })
 }
 
 export default async function updateAccount(this: Wallet) {
   try {
     this.error = null
     this.loading = { ...this.loading, update: true }
-    await this.server
+    let account = await this.server
       .accounts()
       .accountId(this.account.publicKey)
       .call()
-      .then((account) => {
-        this.account = {
-          ...this.account,
-          state: loOmit(account, [
-            'id',
-            '_links',
-            'account_id',
-            'paging_token',
-          ]),
-        }
-      })
-      .finally(() =>
-        claimableBalanceQuery(this).finally(() => {
-          this.loading = { ...this.loading, update: false }
-        })
-      )
+    this.account = {
+      ...this.account,
+      state: loOmit(account, ['id', '_links', 'account_id', 'paging_token']),
+    }
+    await claimableBalanceQuery(this)
+    this.loading = { ...this.loading, update: false }
   } catch (err) {
     this.error = handleError(err)
   }
