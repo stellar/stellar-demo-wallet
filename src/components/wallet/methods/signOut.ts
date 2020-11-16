8f4ee0dcaada06f4d6099d7d3d0024e2fa1be8fe
Jake Urban <jake@stellar.org>
2020-11-16 08:55:25 -0800

    add back prompt for sign out

--- src/components/wallet/methods/signOut.ts
+++ src/components/wallet/methods/signOut.ts
@@ -1,9 +1,15 @@
 import { remove } from '@services/storage'
 import { handleError } from '@services/error'
 import { Wallet } from '../wallet'
+import { PromptInput } from '@prompt/promptInput'
 
 export default async function signOut(this: Wallet) {
   try {
+    await this.setPrompt({
+      message: 'You can press the back button to sign back in.',
+      inputs: [], // No inputs, by default it displays an empty text field
+    })
+
     // flush browser storage
     await remove('WALLET[keystore]')
     // remove secretKey param if present and reload
