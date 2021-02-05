a2906b6ac2cc68e105ddbc57ed14f5896eb2f654
Iveta <quietbits@gmail.com>
2021-02-05 13:13:07 -0500

    Fix search query params

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,13 +1,25 @@
-import { Route, Redirect, RouteProps } from "react-router-dom";
+import { Route, Redirect, RouteProps, useLocation } from "react-router-dom";
 import { useRedux } from "hooks/useRedux";
 
 export const PrivateRoute = ({ children, ...rest }: RouteProps) => {
   const { account } = useRedux("account");
+  const location = useLocation();
 
   return (
     <Route
       {...rest}
-      render={() => (account.isAuthenticated ? children : <Redirect to="/" />)}
+      render={() =>
+        account.isAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/",
+              search: location.search,
+            }}
+          />
+        )
+      }
     />
   );
 };
