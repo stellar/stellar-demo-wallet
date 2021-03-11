f9c733cf18249d6b79e3dfc0dc65e2a7cf59f1a4
Iveta <quietbits@users.noreply.github.com>
2021-03-11 12:56:49 -0500

    Sign out confirmation (#117)
    * Sign out confirmation
    
    * Cleanup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,33 @@
-import React from "react";
+import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { ProjectLogo, TextButton } from "@stellar/design-system";
+import {
+  Button,
+  ButtonVariant,
+  ProjectLogo,
+  TextButton,
+} from "@stellar/design-system";
 import { resetStoreAction } from "config/store";
+import { Modal } from "components/Modal";
 import { useRedux } from "hooks/useRedux";
 
 export const Header = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
   const { account } = useRedux("account");
   const dispatch = useDispatch();
   const history = useHistory();
+
+  const handleCloseModal = () => {
+    setModalVisible(false);
+  };
 
   const handleSignOut = () => {
     dispatch(resetStoreAction());
     history.push({
       pathname: "/",
     });
+    handleCloseModal();
   };
 
   return (
@@ -22,9 +35,27 @@ export const Header = () => {
       <div className="Inset">
         <ProjectLogo title="Demo Wallet" />
         {account.isAuthenticated && (
-          <TextButton onClick={handleSignOut}>Sign out</TextButton>
+          <TextButton onClick={() => setModalVisible(true)}>
+            Sign out
+          </TextButton>
         )}
       </div>
+
+      <Modal visible={modalVisible} onClose={handleCloseModal}>
+        <div className="ModalBody">
+          <p>
+            You can reload the account using your secret key or press back in
+            your browser to sign back in.
+          </p>
+        </div>
+
+        <div className="ModalButtonsFooter">
+          <Button onClick={handleSignOut}>Sign out</Button>
+          <Button variant={ButtonVariant.secondary} onClick={handleCloseModal}>
+            Go back
+          </Button>
+        </div>
+      </Modal>
     </div>
   );
 };
