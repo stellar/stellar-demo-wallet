5be825566c0d8a43e4bee36d14a97db7db2a9965
Marcelo Salloum dos Santos <marcelo@stellar.org>
2021-07-16 17:41:25 -0300

    Show a list of suggested assets on testnet (#206)
    ### What
    
    Provide known assets from the Stellar testnet to help users get started:
    * `USDC:GBBD47IF6LWK7P7MDEVSCWR7DPUWV3NY3DTQEVFL4NAT4AQH3ZLLFLA5`
    * `SRT:testanchor.stellar.org`
    * `MYASSET:sep8-server.dev.stellar.org`
    
    Close stellar/stellar-demo-wallet#165
    
    ### Demo
    
    https://user-images.githubusercontent.com/1952597/125978240-a9a0518a-bcae-4e88-9d62-a3dca00a29e3.mov

---
+++ src/components/AddPresetAsset.tsx
@@ -0,0 +1,179 @@
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import {
+  Button,
+  Checkbox,
+  Heading2,
+  InfoBlock,
+  InfoBlockVariant,
+  Loader,
+} from "@stellar/design-system";
+import { TextLink } from "components/TextLink";
+import { getErrorMessage } from "helpers/getErrorMessage";
+import { getNetworkConfig } from "helpers/getNetworkConfig";
+import { getPresetAssets } from "helpers/getPresetAssets";
+import { getValidatedUntrustedAsset } from "helpers/getValidatedUntrustedAsset";
+import { log } from "helpers/log";
+import { searchParam } from "helpers/searchParam";
+import { useRedux } from "hooks/useRedux";
+import { ActionStatus, presetAsset, SearchParams } from "types/types.d";
+import { shortenStellarKey } from "helpers/shortenStellarKey";
+
+export const AddPresetAsset = ({ onClose }: { onClose: () => void }) => {
+  const { account, allAssets, settings, untrustedAssets } = useRedux(
+    "account",
+    "allAssets",
+    "settings",
+    "untrustedAssets",
+  );
+  const [presetAssets, setPresetAssets] = useState<presetAsset[]>([]);
+  const [checkedAssets, setCheckedAssets] = useState<{
+    [key: string]: boolean;
+  }>({});
+  const [isValidating, setIsValidating] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const history = useHistory();
+
+  useEffect(() => {
+    setPresetAssets(getPresetAssets(allAssets.data));
+  }, [allAssets]);
+
+  useEffect(() => {
+    if (untrustedAssets.status === ActionStatus.SUCCESS) {
+      onClose();
+    }
+
+    if (untrustedAssets.errorString) {
+      setErrorMessage(untrustedAssets.errorString);
+    }
+  }, [onClose, untrustedAssets.errorString, untrustedAssets.status]);
+
+  const getAssetId = (asset: presetAsset) =>
+    `${asset.assetCode}:${asset.homeDomain || asset.issuerPublicKey}`;
+
+  const hasAnySelectedAsset = () =>
+    Object.values(checkedAssets).some((isChecked) => isChecked === true);
+
+  const handleAddUntrustedAssets = async (assetList: presetAsset[]) => {
+    setErrorMessage("");
+    setIsValidating(true);
+
+    try {
+      const validatedAssetValues = await Promise.all(
+        assetList.map(async (pAsset) => {
+          const { assetCode, issuerPublicKey, homeDomain } = pAsset;
+
+          if (!(homeDomain || issuerPublicKey)) {
+            const errorMsg = `Home domain OR issuer public key is required with asset code ${assetCode}`;
+            throw new Error(errorMsg);
+          }
+
+          const asset = await getValidatedUntrustedAsset({
+            assetCode,
+            homeDomain,
+            issuerPublicKey,
+            accountBalances: account.data?.balances,
+            networkUrl: getNetworkConfig(settings.pubnet).url,
+          });
+
+          return `${asset.assetCode}:${asset.assetIssuer}`;
+        }),
+      );
+
+      const newSearchQ = searchParam.update(
+        SearchParams.UNTRUSTED_ASSETS,
+        validatedAssetValues.join(","),
+      );
+      history.push(newSearchQ);
+    } catch (e) {
+      const errorMsg = getErrorMessage(e);
+      log.error({ title: errorMsg });
+      setErrorMessage(errorMsg);
+      setIsValidating(false);
+      return;
+    }
+
+    setIsValidating(false);
+  };
+
+  const isPending =
+    isValidating || untrustedAssets.status === ActionStatus.PENDING;
+
+  const renderAssetRow = (asset: presetAsset) => {
+    const { homeDomain, issuerPublicKey: assetIssuer } = asset;
+
+    const assetId = getAssetId(asset);
+    const networkUrl = getNetworkConfig(settings.pubnet).url.replace(
+      "https:",
+      "",
+    );
+
+    // if no home domain is provided, use horizon's /account endpoint:
+    const issuerLink =
+      (homeDomain && `//${homeDomain}/.well-known/stellar.toml`) ||
+      (assetIssuer && `${networkUrl}/accounts/${assetIssuer}`);
+    const displayLink =
+      homeDomain || (assetIssuer && shortenStellarKey(assetIssuer));
+
+    return (
+      <div key={`preset-asset-${assetId}`} className="PresetAssetRow">
+        <Checkbox
+          id={assetId}
+          label=""
+          checked={Boolean(checkedAssets[assetId])}
+          onChange={() => {
+            const updatedCheckedAssets = { ...checkedAssets };
+            updatedCheckedAssets[assetId] = !checkedAssets[assetId];
+            setCheckedAssets(updatedCheckedAssets);
+          }}
+          disabled={isPending}
+        />
+        <div>
+          <div className="PresetAssetCode">{asset.assetCode}</div>
+          <div className="PresetAssetIssuer">
+            {displayLink && (
+              <TextLink href={issuerLink} isExternal>
+                {displayLink}
+              </TextLink>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <Heading2 className="ModalHeading">Add preset asset</Heading2>
+
+      <div className="ModalBody">
+        <p>Select one or more assets</p>
+        <div className="PresetAssets">{presetAssets.map(renderAssetRow)}</div>
+
+        {errorMessage && (
+          <InfoBlock variant={InfoBlockVariant.error}>
+            <p>{errorMessage}</p>
+          </InfoBlock>
+        )}
+      </div>
+
+      <div className="ModalButtonsFooter">
+        {isPending && <Loader />}
+
+        <Button
+          onClick={() => {
+            const assetsToAdd = presetAssets.flatMap((pAsset) => {
+              const assetId = getAssetId(pAsset);
+              return checkedAssets[assetId] ? pAsset : [];
+            });
+            handleAddUntrustedAssets(assetsToAdd);
+          }}
+          disabled={isPending || !hasAnySelectedAsset()}
+        >
+          Confirm
+        </Button>
+      </div>
+    </>
+  );
+};
