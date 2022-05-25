5b8efa5ddd04b54952edebf06635e12be2297e59
Iveta <quietbits@gmail.com>
2022-05-25 15:51:17 -0400

    Custodial fields UI functional

--- packages/demo-wallet-client/src/components/ConfirmAssetAction.tsx
+++ packages/demo-wallet-client/src/components/ConfirmAssetAction.tsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import { Button, Modal } from "@stellar/design-system";
 import { useRedux } from "hooks/useRedux";
 import { CustodialFields } from "components/CustodialFields";
 
 export const ConfirmAssetAction = ({ onClose }: { onClose: () => void }) => {
   const { activeAsset } = useRedux("activeAsset");
+  const [isCustodialFieldsValid, setIsCustodialFieldsValid] = useState(true);
 
   if (!activeAsset?.action) {
     return null;
@@ -11,6 +13,8 @@ export const ConfirmAssetAction = ({ onClose }: { onClose: () => void }) => {
 
   const { title, description, callback, options, showCustodial } =
     activeAsset.action;
+
+  const isStartDisabled = showCustodial && !isCustodialFieldsValid;
 
   return (
     <>
@@ -24,7 +28,11 @@ export const ConfirmAssetAction = ({ onClose }: { onClose: () => void }) => {
             description
           ))}
         {options ? <p>{options}</p> : null}
-        {showCustodial ? <CustodialFields /> : null}
+        {showCustodial ? (
+          <CustodialFields
+            isValid={(isValid: boolean) => setIsCustodialFieldsValid(isValid)}
+          />
+        ) : null}
       </Modal.Body>
 
       <Modal.Footer>
@@ -32,6 +40,7 @@ export const ConfirmAssetAction = ({ onClose }: { onClose: () => void }) => {
           onClick={() => {
             callback();
           }}
+          disabled={isStartDisabled}
         >
           Start
         </Button>
--- packages/demo-wallet-client/src/components/CustodialFields/index.tsx
+++ packages/demo-wallet-client/src/components/CustodialFields/index.tsx
@@ -1,5 +1,100 @@
+import { useState, useEffect } from "react";
+import { Checkbox, Input } from "@stellar/design-system";
+import { Memo, StrKey } from "stellar-sdk";
 import "./styles.scss";
 
-export const CustodialFields = () => {
-  return <div className="CustodialFields">Custodial options</div>;
+export const CustodialFields = ({
+  isValid,
+}: {
+  isValid: (isValid: boolean) => void;
+}) => {
+  const [isCustodialMode, setIsCustodialMode] = useState(false);
+  const [secretKeyValue, setSecretKeyValue] = useState("");
+  const [memoIdValue, setMemoIdValue] = useState("");
+  const [errorSecretKey, setErrorSecretKey] = useState("");
+  const [errorMemoId, setErrorMemoId] = useState("");
+
+  const isInputsValid =
+    !!secretKeyValue && !errorSecretKey && !!memoIdValue && !errorMemoId;
+
+  useEffect(() => {
+    if (isCustodialMode) {
+      isValid(isInputsValid);
+    } else {
+      isValid(true);
+    }
+  }, [isCustodialMode, isInputsValid, isValid]);
+
+  const resetLocalState = () => {
+    setIsCustodialMode(false);
+    setSecretKeyValue("");
+    setMemoIdValue("");
+    setErrorSecretKey("");
+    setErrorMemoId("");
+  };
+
+  const handleCustodialModeChange = () => {
+    if (isCustodialMode) {
+      resetLocalState();
+    }
+
+    setIsCustodialMode(!isCustodialMode);
+  };
+
+  const validateSecretKey = (value: string) => {
+    setErrorSecretKey("");
+    const isSecretKeyValid = StrKey.isValidEd25519SecretSeed(value);
+
+    if (!isSecretKeyValid) {
+      setErrorSecretKey("Secret key is not valid");
+    }
+
+    // TODO: set secret key in store
+  };
+
+  const validateMemoId = (value: string) => {
+    setErrorMemoId("");
+
+    try {
+      const memoId = Memo.id(value);
+      // TODO: set memo id in store
+      console.log(">>> memoId: ", memoId);
+    } catch (e) {
+      setErrorMemoId("Memo ID must be a valid 64 bit unsigned integer");
+    }
+  };
+
+  return (
+    <div className="CustodialFields">
+      <Checkbox
+        id="custodialMode"
+        label="Custodial mode"
+        onChange={handleCustodialModeChange}
+      />
+
+      {isCustodialMode ? (
+        <div className="CustodialFields__container">
+          <Input
+            id="custodialSecretKey"
+            label="Custodial secret key"
+            placeholder="Starts with S, example: SCHK…ZLJK"
+            onBlur={(e) => {
+              setSecretKeyValue(e.target.value);
+              validateSecretKey(e.target.value);
+            }}
+            error={errorSecretKey}
+          />
+          <Input
+            id="custodialMemoId"
+            label="Custodial Memo ID"
+            onBlur={(e) => {
+              setMemoIdValue(e.target.value);
+              validateMemoId(e.target.value);
+            }}
+            error={errorMemoId}
+          />
+        </div>
+      ) : null}
+    </div>
+  );
 };
