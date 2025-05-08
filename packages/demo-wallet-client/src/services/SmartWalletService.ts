e482da39937530e2082dbd09385d401e21bff9b3
Jiahui Hu <jiahuihu0425@gmail.com>
2025-05-08 13:28:43 -0600

    [ANCHOR-1023] connect passkey contract (#392)
    * connect passkey contract
    
    * comments

--- packages/demo-wallet-client/src/services/SmartWalletService.ts
+++ packages/demo-wallet-client/src/services/SmartWalletService.ts
@@ -1,14 +1,14 @@
 import { PasskeyService } from "./PasskeyService";
-import { Keypair } from "@stellar/stellar-sdk";
+import { Address, hash, Keypair, StrKey, xdr } from "@stellar/stellar-sdk";
 // @ts-ignore
 import { Client, basicNodeSigner } from "@stellar/stellar-sdk/contract";
-// @ts-ignore
-import { Server } from "@stellar/stellar-sdk/rpc";
-import { SOROBAN_CONFIG } from "../config/constants";
+import { SOROBAN_CONFIG, SOURCE_KEYPAIR_SECRET } from "../config/constants";
+import base64url from "base64url";
 
 export class SmartWalletService {
   private static instance: SmartWalletService;
   private passkeyService: PasskeyService;
+  private readonly sourceKeypair: Keypair;
 
   public static getInstance(): SmartWalletService {
     if (!SmartWalletService.instance) {
@@ -19,18 +19,15 @@ export class SmartWalletService {
 
   constructor() {
     this.passkeyService = new PasskeyService();
+    this.sourceKeypair = Keypair.fromSecret(SOURCE_KEYPAIR_SECRET);
   }
 
   public async createPasskeyContract(passkeyName: string) {
     // 1. Register passkey
     const { pkId, pk } = await this.passkeyService.registerPasskey(passkeyName);
 
-    // 2. Generate source account
-    const sourceKeypair = await this.generateFundedKeypair();
-
-    // 3. Deploy contract
-    const { signTransaction } = basicNodeSigner(sourceKeypair, SOROBAN_CONFIG.NETWORK_PASSPHRASE);
-
+    // 2. Deploy contract
+    const { signTransaction } = basicNodeSigner(this.sourceKeypair, SOROBAN_CONFIG.NETWORK_PASSPHRASE);
     const deployTx = await Client.deploy(
       {
         credential_id: pkId,
@@ -40,36 +37,54 @@ export class SmartWalletService {
         networkPassphrase: SOROBAN_CONFIG.NETWORK_PASSPHRASE,
         rpcUrl: SOROBAN_CONFIG.RPC_URL,
         wasmHash: SOROBAN_CONFIG.WASM_HASH,
-        publicKey: sourceKeypair.publicKey(),
+        salt: hash(base64url.toBuffer(pkId)),
+        publicKey: this.sourceKeypair.publicKey(),
         signTransaction,
       }
     );
 
     const { result: client } = await deployTx.signAndSend();
     const contractId = client.options.contractId;
-
-    console.log("Contract Deployment Info:", {
-      contractId,
-      sourceAccount: {
-        publicKey: sourceKeypair.publicKey(),
-        secret: sourceKeypair.secret(),
-      },
-    });
+    console.log("Contract Deployed: " + contractId);
 
     return {
       contractId,
       pkId,
-      sourceAccount: {
-        publicKey: sourceKeypair.publicKey(),
-        secretKey: sourceKeypair.secret(),
-      },
     };
   }
 
-  private async generateFundedKeypair() {
-    const keypair = Keypair.random();
-    const server = new Server(SOROBAN_CONFIG.RPC_URL);
-    await server.requestAirdrop(keypair.publicKey());
-    return keypair;
+  public async connectPasskeyContract() {
+    // 1. connect passkey
+    const pkId = await this.passkeyService.connectPasskey();
+    if (!pkId) {
+      throw new Error("No `keyId` was found");
+    }
+
+    // 2. retrieve the contractId through the networkId, pkId, and deployer address
+    const preimage = xdr.HashIdPreimage.envelopeTypeContractId(
+      new xdr.HashIdPreimageContractId({
+        networkId: hash(Buffer.from(SOROBAN_CONFIG.NETWORK_PASSPHRASE,)),
+        contractIdPreimage: xdr.ContractIdPreimage.contractIdPreimageFromAddress(
+          new xdr.ContractIdPreimageFromAddress({
+            address: Address.fromString(this.sourceKeypair.publicKey()).toScAddress(),
+            salt: hash(base64url.toBuffer(pkId)),
+          }),
+        ),
+      }),
+    );
+    const contractId = StrKey.encodeContract(hash(preimage.toXDR()));
+    console.log("Contract Connected: " + contractId);
+
+    return {
+      contractId,
+      pkId,
+    }
   }
+
+  // private async generateFundedKeypair() {
+  //   const keypair = Keypair.random();
+  //   const server = new Server(SOROBAN_CONFIG.RPC_URL);
+  //   await server.requestAirdrop(keypair.publicKey());
+  //   return keypair;
+  // }
 }
