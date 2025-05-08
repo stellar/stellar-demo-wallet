0b617a4a352ada55f8eaca9093d0c6ec23bf40cf
Jiahui Hu <jiahuihu0425@gmail.com>
2025-05-08 13:28:43 -0600

    [ANCHOR-1023] connect passkey contract (#392)
    * connect passkey contract
    
    * comments

--- packages/demo-wallet-client/src/ducks/contractAccount.ts
+++ packages/demo-wallet-client/src/ducks/contractAccount.ts
@@ -1,12 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { AccountKeyPair, RejectMessage } from "../types/types";
+import { RejectMessage } from "../types/types";
 import { RootState } from "../config/store";
 import { log } from "demo-wallet-shared/build/helpers/log";
 import { getErrorMessage } from "demo-wallet-shared/build/helpers/getErrorMessage";
 import { SmartWalletService } from "../services/SmartWalletService";
 
 export const createPasskeyContract = createAsyncThunk<
-  { contractId: string; keyId: string; sourceAccount: AccountKeyPair },
+  { contractId: string; keyId: string; },
   string,
   { rejectValue: RejectMessage; state: RootState }
 >("contractAccount/createPasskeyContract", async (passkeyName, { rejectWithValue }) => {
@@ -17,7 +17,6 @@ export const createPasskeyContract = createAsyncThunk<
     return {
       contractId: result.contractId,
       keyId: result.pkId,
-      sourceAccount: result.sourceAccount,
     };
   } catch (error) {
     log.error({
@@ -30,3 +29,31 @@ export const createPasskeyContract = createAsyncThunk<
     });
   }
 });
+
+export const connectPasskeyContract = createAsyncThunk<
+  { contractId: string; keyId: string; },
+  void,
+  { rejectValue: RejectMessage; state: RootState }
+>(
+  "contractAccount/connectPasskeyContract",
+  async (_, { rejectWithValue }) => {
+    try {
+      log.instruction({ title: "Connecting contract" });
+      const swService = SmartWalletService.getInstance();
+      const result = await swService.connectPasskeyContract();
+      return {
+        contractId: result.contractId,
+        keyId: result.pkId,
+      };
+    } catch (error) {
+      log.error({
+        title: "Connecting contract failed",
+        body: getErrorMessage(error),
+      });
+      return rejectWithValue({
+        errorString:
+          "Something went wrong while connecting account, please try again.",
+      });
+    }
+  }
+);
