324aea6cd283dd42c280528c627e7bafdec00725
jiahuihu <jiahuihu0425@gmail.com>
2025-08-12 10:19:48 -0400

    trigger preview rebuild

--- packages/demo-wallet-client/webpack.dev.js
+++ packages/demo-wallet-client/webpack.dev.js
@@ -3,7 +3,7 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: "development",
+  mode: "production",
   devtool: "cheap-module-source-map",
   devServer: {
     static: path.join(__dirname, "build"),
