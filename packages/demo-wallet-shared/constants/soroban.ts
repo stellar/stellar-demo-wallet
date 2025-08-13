5a97a4068880a09f9b548ae43621d3e2e15c7c99
Jiahui Hu <jiahuihu0425@gmail.com>
2025-08-13 14:51:50 -0600

    [ANCHOR-1072] Improve check auth in contract  (#404)
    * improve check auth
    
    * comments

--- packages/demo-wallet-shared/constants/soroban.ts
+++ packages/demo-wallet-shared/constants/soroban.ts
@@ -3,7 +3,7 @@ import { Api } from "@stellar/stellar-sdk/rpc";
 export const SOROBAN_CONFIG = {
   MAX_FEE: "10000",
   ONE: 10 ** 7, // Stroops in 1
-  WASM_HASH: "03dbb8b88b981e944ae44f48edba5a39c8351ea8c84959b92707108837654f6f",
+  WASM_HASH: "81d56ff8e2d8ea10a8ddafd68265fc51bf5a3838e775a06c3d0f94ba52c8095f",
 } as const;
 
 export const SendTxStatus: {
